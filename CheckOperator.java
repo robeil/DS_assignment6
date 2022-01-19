c64e32cf5dd9c4f9b670577aaffd0b3e9be0f1c9
Robeil Aregawi <robeilaregawi1@gmail.com>
2022-01-18 18:07:41 -0600

    Starting Question # 1

---
+++ CheckOperator.java
@@ -0,0 +1,13 @@
+package CS221Course.src.DS_assignment6;
+
+public class CheckOperator {
+    /**
+     * Design a Balanced Symbols Check operator, it should do the following:
+     * - Read a mathematical expression from the user
+     * - Check and report whether the expression is balanced
+     * - {, }, (, ), [, ] are the only symbols considered for the check.
+     * All other characters can be ignored.
+     *
+     */
+
+}
